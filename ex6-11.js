ffab52ed4a27c0eb0db613c0c85a9ee6ab6332bd
kosukeyoshida1262 <zm19453@josai.ac.jp>
2022-02-02 07:10:24 +0000

    0202c

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
-function imgchange21() {document.getElementById('kihara').src='./image/';}
-function imgchange22() {document.getElementById('kihara').src='./image/';}
+function imgchange21() {document.getElementById('kihara').src='./image/kihara.kyoto.jpg';}
+function imgchange22() {document.getElementById('kihara').src='./image/kihara.urawa.jpg';}
--- ex6-12.js
+++ ex6-12.js
@@ -1,2 +1,2 @@
-function imgchange23() {document.getElementById('kudo').src='./image/';}
-function imgchange24() {document.getElementById('kudo').src='./image/';}
+function imgchange23() {document.getElementById('kudo').src='./image/kudo.jy.jpg';}
+function imgchange24() {document.getElementById('kudo').src='./image/kudo.urawa.jpg';}
